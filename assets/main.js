fe25439bf7e0eb27e252bf71d794ff932575a35a
kimani-ndegwa <kimani@mavenmachines.com>
2020-02-03 16:40:23 +0300

    Add in language set. Also allow the error messages to be legitimate

--- assets/main.js
+++ assets/main.js
@@ -1,90 +1,112 @@
 (
     function(){
+        const API_URL_SUFFIX = "wikipedia.org/api/rest_v1/page/metadata/";
         const input = document.getElementById("title-input");
         const loadingText = document.getElementsByClassName("loading-text")[0];
+        const info = document.getElementsByClassName("info")[0];
+        const error = document.getElementsByClassName("error")[0];
         const submitArticleTitleBtn = document.getElementById("submit-article-title");
-        const BASE_API_URL = "https://en.wikipedia.org/api/rest_v1/page/metadata/";
+        // Language Selection.
         const LANGUAGES = [
             {
                 key: 'en',
-                language: "English"
-            }
+                language: "English",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'fr',
+                language: "Français",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'it',
+                language: "Italiano",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'pt',
+                language: "Português",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'pl',
+                language: "Polski",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'zh',
+                language: "中文",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'de',
+                language: "Deutsch",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
-            }
+                key: 'es',
+                language: "Deutsch",
+                dir:"ltr"
+            },
             {
-                key: 'en',
-                language: "English"
+                key: 'ar',
+                language: 'العربية',
+                dir:"rtl"
             }
-            {
-                key: 'en',
-                language: "English"
-            }
-        ]
+        ];
+        // Select Language Paragraph
+        const selectLanguageParagraph = document.getElementsByClassName("select-language-section")[0];
+        // Insert Languages Dynamically in the DOM
+        const options = LANGUAGES.map(lang=>{
+            return `<option value=${lang.key} dir=${lang.dir}>${lang.language}</option>`
+        });
 
+        // Note. InnerHTML is expensive. For consideration later.
+        selectLanguageParagraph.innerHTML = `<select id="language-options">${options}</select>`;
         submitArticleTitleBtn.addEventListener("click", ()=>{
             // Show loader text
             loadingText.classList.remove('hide');
+
+            // Get Language
+            const lang = document.getElementById("language-options").value;
             const query = input.value;
             if(query.length){
-            fetch(`${BASE_API_URL}${query}`)
+            fetch(`https://${lang}.${API_URL_SUFFIX}${query}`)
             .then(data=>{
-                console.log(data)
                if(data.ok){
                     data
                     .json()
                     .then(info=>{
+                        error.classList.add('hide'); // Info Text Hide
                         loadingText.classList.add('hide');
                         const resultsSection = document.getElementById('results-section');
                         const data = info.toc.entries.filter(item=>item.html);
                         let text = '';
                         for(let i=0; i < data.length; i++){
-                            text = text + `<li> ${data[i].html} <li/>`
+                            console.log(data[i], "here we go")
+                            text = text + `<p>${data[i].number}. ${data[i].html}</p>`
                         };
                         resultsSection.innerHTML = `
-                        <ul>
+                            <div class="results-list">
                             ${text}
-                        </ul>
+                            </div>
                         `
                         loading = false;
                     });
     
                } else {
-                    loadingText.classList.add('hide');
+                   loadingText.classList.add('hide');
                    throw new Error("Data response not a 200")
                }
             })
             .catch(error=>{
                 loadingText.classList.add('hide');
+                error.classList.remove('hide');
                 console.log(error, "error occured")
             });
             } else {
                 loadingText.classList.add('hide');
+                info.classList.remove('hide');
                 console.log("Nothing submitted")
             }
         });
